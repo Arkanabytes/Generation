be432fae7d16a317df440e66be27effbf8531878
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-06-16 16:20:45 -0400

    Delete ProyectoJavaDay3/C20160625 directory

--- ProyectoJavaDay3/C20160625/IfBoolean.java
+++
@@ -1,8 +0,0 @@
-package C20160625;
-
-public class IfBoolean {
-    public static void main(String[] args) {
-        boolean esVerdadero = true;
-        if (esVerdadero) System.out.println("Verdadero");
-    }
-}
