5e293f6d7fd8cc21ea816b7108d690007f2f305c
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-06-16 18:03:12 -0400

    Add files via upload

---
+++ ProyectoJavaDay3/src/C20160625/For.java
@@ -0,0 +1,12 @@
+package C20160625;
+
+public class For {
+    public static void main(String[] args) {
+        System.out.println("Contador Basico");
+        System.out.println("Contador del 1 al 10");
+         for (int i = 0; i <10; i++){
+             System.out.println(i + " ");
+
+      }
+    }
+}
