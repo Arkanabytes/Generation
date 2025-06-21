d5af812d801f47bfaaadec5368f4a0b265fc142f
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-06-20 22:40:56 -0400

    Strings.java

--- ProyectoJavaDay6/src/C20190625/Strings.java
+++ ProyectoJavaDay6/src/C20190625/Strings.java
@@ -4,16 +4,14 @@ public class Strings {
     public static void main(String[] args) {
         // Vamos hacer referencias a los textos de java.lang
         String text = "Hola Mundo";
-        System.out.println("Longitud" + text.length());
-        System.out.println("Primer caracter" + text.charAt(0));
-        System.out.println("Ultimo caracter" + text.charAt(text.length()-1));
-        System.out.println("Primer caracter" + text.toUpperCase());
-        System.out.println("Longitud" + text.toLowerCase());
-        System.out.println("Primer caracter" + text.trim());
-        System.out.println("Primer caracter" + text.trim().toLowerCase());
-        System.out.println("Primer caracter" + text.trim().toUpperCase());
 
-        System.out.println("Ultimo caracter" + text.charAt(text.length()-1));
-        System.out.println("Primer caracter" + text.charAt(0));
+        System.out.println("Longitud: " + texto.length());
+        System.out.println("Primer carácter: " + texto.charAt(0));
+        System.out.println("Subcadena (0 a 4): " + texto.substring(0, 4));
+        System.out.println("¿Contiene 'Mundo'? " + texto.indexOf("Mundo"));
+        System.out.println("Concatenado: " + texto.concat(" desde Java"));
+        System.out.println("¿Es igual a 'hola mundo'? " + texto.equals("hola mundo"));
+        System.out.println("Mayúsculas: " + texto.toUpperCase());
+        System.out.println("Minúsculas: " + texto.toLowerCase());
     }
 }
