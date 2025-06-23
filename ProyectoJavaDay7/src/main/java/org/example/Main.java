a57c16f612db1d88263b4163cbfec4cf48dd121b
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-06-23 14:57:48 -0400

    Update Main.java

--- ProyectoJavaDay7/src/main/java/org/example/Main.java
+++ ProyectoJavaDay7/src/main/java/org/example/Main.java
@@ -1,17 +1,75 @@
-package org.example;
+// Array
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+package semana5.array;
+
+public class Array {
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // DECLARACION DE UN ARRAY ES FIJA
+        // UN ARRAY TIENE QUE USAR []
+        // sintaxis : tipo[] nombre;
+        int[] numeros = new int[5];
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        numeros[0] = 10;
+        numeros[1] = 20;
+        numeros[2] = 30;
+        numeros[3] = 40;
+        numeros[4] = 50;
+
+        System.out.println(numeros[0]); // va imprimir el valor de la variable numero en la posicion [0] que seria 10
+        System.out.println(numeros[1]);
+        System.out.println(numeros.length); // imprimira la longitud del arreglo que es 5
+
+        System.out.println("\nEjemplo Arreglo con For");
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Indice " + i + " : " + numeros[i]);
         }
+
+        String[] frutas = {"Manzana", "Pera", "Uva"}; // Longitud 0 1 2  = 3
+
+        System.out.println("\nFrutas: ");
+        // Basicamente diremos que iremos elemento por elemento
+        for (String fruta : frutas) {
+            System.out.print(fruta + " ");
+         //    System.out.println(frutas);
+            // NO imprimir directamente el arreglo, nisiera dentro del for each
+            // [Ljava.lang.String;@52cc8049  <--- se ve asi
+        }
+        // {"Manzana", "Pera", "Uva"}
+        // luego del for each es : Manzana Pera Uva
+
+        // Por que no usamos esto asi
+        // Esta técnica no es tan limpia, pero funciona si por alguna razón necesitas el estilo for-each y el índice.
+ /*       int index = 0;
+        for (String fruta : frutas) {
+            if (fruta.equals("Manzana")) {
+                System.out.println("En el índice: " + index);
+            }
+            index++;
+        }
+        */
+
+        // Arreglos con Boleanos
+
+        boolean[] respuestas = new boolean[5]; // Por defecto en false
+        respuestas[0] = true;
+        respuestas[4] = true;
+        
+        System.out.println("\nRespuestas:");
+        for (boolean respuesta : respuestas) {
+            System.out.println("Valor: " + respuesta);
+        }
+
+        for( int i = 0; i < respuestas.length; i++){
+            if(respuestas[i]){
+                System.out.println("La respuesta " + i + " es verdadera");
+            }else{
+                System.out.println("La respuesta " + i + " es falso");
+            }
+        }
+
+        
+
     }
 }
