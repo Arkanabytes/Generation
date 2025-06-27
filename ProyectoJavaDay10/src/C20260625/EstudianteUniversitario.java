d8f92dfe770802109c434926a8b2e605e3310fe0
Alejandra Pinto <consuelo.pinto02@inacapmail.cl>
2025-06-27 17:28:52 -0400

    Add files via upload

---
+++ ProyectoJavaDay10/src/C20260625/EstudianteUniversitario.java
@@ -0,0 +1,19 @@
+package C20260625;
+import java.lang.String;
+
+public class EstudianteUniversitario extends Estudiante{
+
+    private String carrera;
+    private int creditos;
+
+    public EstudianteUniversitario(String nombre, int edad, String carrera, int creditos) {
+        super(nombre,edad);
+        this.carrera = carrera;
+        this.creditos = creditos;
+    }
+
+    @Override
+    public void estudiar() {
+        System.out.println("Estudiante " + nombre + " esta estudiando en " + carrera + " con " + creditos + " creditos");
+    }
+}
