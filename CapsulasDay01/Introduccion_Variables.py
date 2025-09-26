8b0781c47d1fe1832eb0d2b276ab66ec9d34caa1
Alejandra Pinto <konshuelo@hotmail.com>
2025-09-26 16:50:52 -0300

    Create Introduccion_Variables.py

---
+++ CapsulasDay01/Introduccion_Variables.py
@@ -0,0 +1,57 @@
+# Contexto
+# Tipos de variable
+
+# int, float, str, bool
+
+#definir una varible
+nombre = "Isra";
+edad = 45;
+
+#1
+#"1"
+# "one"
+
+uno_str = "uno"
+uno_int = 1
+uno_float = 1.0
+uno_char = '1'
+
+numero_dig_uno = 1
+numero_char = "1"
+numero_str = "uno"
+
+#operadores
+#aritmeticos + - / *
+
+#comparacion => siempre es resultado tipo booleano
+# == != > < >= <=
+
+#% el modulo o es de la division de dos numeros
+
+numero = 5
+
+if numero % 2 == 0:
+  print(f"El número {numero} es par.")
+else:
+  print(f"El número {numero} es impar.")
+  
+  
+numero = 7
+
+if numero & 1:
+    print("Es impar")
+else:
+    print("Es par")
+    
+#operadores logicos
+# and; or; not
+
+edad = int(input("Ingresa tu edad: "))
+
+# Verificar si es mayor o menor de edad
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.")
+    
+# Estrucura de datos, tuplas, diccionarios, listas, Api,Json, XML, MATRICES
